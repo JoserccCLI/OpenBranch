1242452bfba6a874a7e9b00aa8b7ce36bec0cb53
张行 <zhanghang@globalegrow.com>
2019-12-31 09:04:46 +0800

    change command

--- Sources/OpenBranch/main.swift
+++ Sources/OpenBranch/main.swift
@@ -2,17 +2,13 @@ import Swiftline
 import Commander
 
 
-Group {
-    $0.command(
-    "branch",
-    Flag("sdk", default: false, description: "是否打开分支中原生 SDK 的工程，默认为 false 不打开")
-    ) { sdk in
-        let openBranch = OpenBranch(sdk:sdk)
-        do {
-            try openBranch.open()
-        } catch let error {
-            print(error.localizedDescription.f.Yellow.b.Red)
-        }
+command {
+    let isSDK = agree("要不要打开原生支付 SDK工程?")
+    let openBranch = OpenBranch(sdk:isSDK)
+    do {
+        try openBranch.open()
+    } catch let error {
+        print(error.localizedDescription.f.Yellow.b.Red)
     }
 }.run()
 
