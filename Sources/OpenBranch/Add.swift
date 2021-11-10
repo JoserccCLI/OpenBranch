376464fcdc7d951098fccfb4ecb966f8e08a9c1f
king <3925@winnermedical.com>
2021-11-10 17:21:56 +0800

    对于http的支持

--- Sources/OpenBranch/Add.swift
+++ Sources/OpenBranch/Add.swift
@@ -9,13 +9,13 @@ import ArgumentParser
 import Foundation
 struct Add: ParsableCommand {
     func run() throws {
-        print("请输入项目SSH地址")
+        print("请输入项目地址")
         guard let sshURL = readLine(), sshURL.count > 0 else {
             throw CleanExit.message("没有输入任何文本")
         }
-        guard sshURL.contains("git@"), sshURL.contains(".git") else {
-            throw CleanExit.message("\(sshURL)不是一个合法的SSH地址")
-        }
+//        guard sshURL.contains("git@"), sshURL.contains(".git") else {
+//            throw CleanExit.message("\(sshURL)不是一个合法的SSH地址")
+//        }
         let name = inputName()
         var configurations = ConfigurationManager.manager.get()
         configurations.append(.init(sourceURL: sshURL, name: name))
